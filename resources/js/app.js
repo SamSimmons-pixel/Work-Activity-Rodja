fd7f21ddb553b8585abab697c17bade68fbe0dbc
WASD <simile64.exe@gmail.com>
2026-08-27 14:21:44 +0700

    oke

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,7 +21,7 @@ window.readAndRedirect = function (notifId, url) {
     const component = window.Livewire?.all()?.find(c => c.name === 'notifications.notification-menu');
 
     if (component) {
-        component.call('markAsRead', notifId, url);
+        component.markAsRead(notifId, url);
     } else if (url) {
         window.location.href = url;
     }
